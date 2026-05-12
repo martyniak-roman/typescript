d1ae860eec5281e8d5c68ca9f4ed01daf896c36f
martyniak-roman <roman.martyniakdev@gmail.com>
2026-05-12 21:29:50 +0300

    add types for 6-12 tasks hw 2

---
+++ hw_2/task11/script.ts
@@ -0,0 +1,9 @@
+interface Book {
+    title: string;
+    pageCount: number;
+    genre: string;
+}
+
+const book:Book = {title:'Clean Code', pageCount: 330, genre:'Programming'}
+const book1:Book = {title:'Clean Coder', pageCount: 330, genre:'Programming'}
+const book2:Book = {title:'English is not easy', pageCount: 330, genre:'English'}
---
+++ hw_2/task7/script.ts
@@ -0,0 +1,7 @@
+const a:number = -3
+
+if (a === 0){
+    console.log('Вірно')
+} else {
+    console.log('Невірно')
+}
