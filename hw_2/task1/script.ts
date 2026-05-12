ab6122dd45b9f69ef8fe061b94b7f36f29db569a
martyniak-roman <roman.martyniakdev@gmail.com>
2026-05-12 19:13:40 +0300

    add types for 1-5 tasks hw 2

---
+++ hw_2/task1/script.ts
@@ -0,0 +1,12 @@
+let time:number = 39;
+
+
+if (time >= 0 && time < 15) {
+    console.log('1');
+} else if (time >= 15 && time < 30) {
+    console.log('2');
+} else if (time >= 30 && time < 45) {
+    console.log(3);
+} else if (time >= 45 && time < 60) {
+    console.log('4');
+}
