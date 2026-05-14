e27e59911a46bae2147fa568095536171d34bd94
martyniak-roman <roman.martyniakdev@gmail.com>
2026-05-14 16:29:50 +0300

    add types for hw 8 tasks

---
+++ hw_8/task1/script.ts
@@ -0,0 +1,20 @@
+type Course = {title:string, monthDuration: number}
+
+let coursesAndDurationArray:Course[] = [
+
+    {title: 'JavaScript Complex', monthDuration: 5},
+
+    {title: 'Java Complex', monthDuration: 6},
+
+    {title: 'Python Complex', monthDuration: 6},
+
+    {title: 'QA Complex', monthDuration: 4},
+
+    {title: 'FullStack', monthDuration: 7},
+
+    {title: 'Frontend', monthDuration: 4}
+
+];
+
+const courses = coursesAndDurationArray.map((course, idx) => ({id:idx + 1, title:course.title, monthDuration: course.monthDuration}))
+console.log(courses)
